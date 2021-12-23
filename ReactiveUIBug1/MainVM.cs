202e5b10411f3b4d1f3063743fbcfeb43973abc8
Claudiu <test@test.com>
2021-12-23 22:06:52 +0100

    Yes

---
+++ ReactiveUIBug1/MainVM.cs
@@ -0,0 +1,23 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Reactive;
+using System.Text;
+
+namespace ReactiveUIBug1
+{
+    public class MainVM
+    {
+        SomeService someService = new SomeService();
+
+        public ReactiveCommand<Unit, Unit> DeleteMedCmd { get; }
+
+        public MainVM()
+        {
+            DeleteMedCmd = ReactiveCommand.Create(() =>
+            {
+                someService.DoStuff();
+            });
+        }
+    }
+}
---
+++ ReactiveUIBug1/SomeService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReactiveUIBug1
+{
+    public class SomeService
+    {
+        public void DoStuff()
+        {
+            Console.WriteLine("Crash");
+            Console.WriteLine("Crash");
+            Console.WriteLine("Crash");
+            Console.WriteLine("Crash");
+            Console.WriteLine("Crash");
+            Console.WriteLine("Crash");
+            Console.WriteLine("Crash");
+
+            var a = 99999;
+        }
+    }
+}
